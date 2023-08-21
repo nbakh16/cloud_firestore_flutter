41a46b4fe1850bec3deab4d3049c73b3b05d5517
Nabil160 <nbakhunjee97@gmail.com>
2023-08-21 20:16:47 +0600

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:cloud_firestore_flutter/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
